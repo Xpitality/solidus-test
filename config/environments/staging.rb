1cb402dbf26cc1f61300032b0b4ae35af427496e
Marko Bošković <bosskovic@gmail.com>
2020-11-20 14:35:26 +0100

    fixes staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -26,12 +26,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.digest = true
+  config.assets.compile = false
+  config.assets.digest = false
   # config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://solidus_test.xptesting.com'
+  config.action_controller.asset_host = 'https://solidus.xptesting.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
