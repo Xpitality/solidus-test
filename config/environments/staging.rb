26d44040e9ce5123a79dc40bc83bc7f042e0e4e6
Marko Bošković <bosskovic@gmail.com>
2020-11-20 15:11:03 +0100

    fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -26,8 +26,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.digest = false
+  config.assets.compile = true
+  config.assets.digest = true
   # config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
