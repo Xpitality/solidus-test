69a4912f101ef62d73d7324bc82b41a18dfedb29
Marko Bošković <bosskovic@gmail.com>
2020-11-20 15:16:16 +0100

    fix?

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -26,9 +26,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.digest = true
-  # config.serve_static_assets = false
+  config.assets.compile = false
+  config.assets.digest = false
+  config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'https://solidus.xptesting.com'
