68f197725b7fb22179f69fe8d3bc965f6ca67664
Marko Bošković <bosskovic@gmail.com>
2020-11-23 12:16:16 +0100

    adds docker-staging.sh

---
+++ docker-staging.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+docker rm $(docker ps -q -a) -f && docker rmi $(docker images -q) -f
+docker network prune -f
+docker volume prune -f
+
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml up --build -d
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm web rails db:create
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm web rails db:migrate
