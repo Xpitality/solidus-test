f48ddeae355fe1de4363aa594df4be75171120ef
Marko Bošković <bosskovic@gmail.com>
2020-11-23 14:50:11 +0100

    updates staging config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -111,4 +111,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+  config.action_controller.default_protect_from_forgery = false
 end
--- docker/docker-staging.sh
+++ docker/docker-staging.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 /home/deployer/bin/docker-compose -f docker-compose-staging.yml up --build -d
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm web rails db:drop
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm web rails db:create
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm web rails db:migrate
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm web rails g solidus:install
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm web rails db:seed
+#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:drop
+#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:create
+#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:migrate
+#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails g solidus:install
+#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:seed
+
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test bundle exec rake assets:precompile
+
+#bin/rails railties:install:migrations
+#bin/rails db:migrate
+#bin/rails db:seed
+#bin/rails spree_sample:load
