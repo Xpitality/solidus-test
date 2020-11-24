f98dcc9bdefc894bf9ae221aba93eb875985785e
Marko Bošković <bosskovic@gmail.com>
2020-11-24 14:57:42 +0100

    docker cleanup

--- docker/docker-staging.sh
+++ docker/docker-staging.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml build
 /home/deployer/bin/docker-compose -f docker-compose-staging.yml up --build -d
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:drop
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:create
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:migrate
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails g solidus:install
-#/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:seed
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:drop
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:create
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:migrate
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails g solidus:install
+/home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test rails db:seed
 
 /home/deployer/bin/docker-compose -f docker-compose-staging.yml run --rm solidus_test bundle exec rake assets:precompile
 
