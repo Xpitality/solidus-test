5ddc1425f4a111a5b71f1bd4922fcc5d0b88e9d2
Alessio Re <NoSync@users.noreply.github.com>
2020-11-26 14:35:52 +0100

    Update staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Allows any xptesting.com hosts
- config.hosts << '.xptesting.com'
+ config.hosts << ".xptesting.com"
   
   # Code is not reloaded between requests.
   config.cache_classes = true
