ce6f0a6f0d134acb63f1a39d381695b6294f0e8a
Marko Bošković <bosskovic@gmail.com>
2021-01-07 16:57:59 +0100

    adds rome timezone

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module SolidusTest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Rome'
   end
 end
