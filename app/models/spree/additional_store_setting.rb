11b702731ebb5cee43d7a84d3f328509bfd4f30b
Marko Bošković <bosskovic@gmail.com>
2021-03-03 16:47:21 +0100

    adds SpreeAdditionalStoreSetting model, migration, adds it to store via decorater

---
+++ app/models/spree/additional_store_setting.rb
@@ -0,0 +1,7 @@
+module Spree
+  class AdditionalStoreSetting < ActiveRecord::Base
+    belongs_to :store, class_name: 'Spree::Store'
+
+    validates :label, presence: true, uniqueness: {scope: :store_id}
+  end
+end
