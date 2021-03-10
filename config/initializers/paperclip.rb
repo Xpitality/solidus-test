65979416224f973930a10f4602de89415e461aa2
Marko Bošković <bosskovic@gmail.com>
2021-03-10 11:31:55 +0100

    uses rails' encrypted credentials

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,13 +1,13 @@
-if ENV['AWS_ACCESS_KEY_ID'] && ENV['AWS_SECRET_ACCESS_KEY']
+if Rails.application.credentials.config[:aws][:s3][:access_key_id] && Rails.application.credentials.config[:aws][:s3][:secret_access_key]
   Paperclip::Attachment.default_options.merge!(
       storage: :fog,
       fog_credentials: {
           provider: 'AWS',
-          aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-          aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-          region: "eu-west-1",
+          aws_access_key_id: Rails.application.credentials.config[:aws][:s3][:access_key_id],
+          aws_secret_access_key: Rails.application.credentials.config[:aws][:s3][:secret_access_key],
+          region: Rails.application.credentials.config[:aws][:s3][:region],
       },
-      fog_directory: 'com-xptesting-solidus'
+      fog_directory: Rails.application.credentials.config[:aws][:s3][:bucket]
   )
 
   Spree::Image.attachment_definitions[:attachment].delete(:url)
@@ -15,21 +15,21 @@ if ENV['AWS_ACCESS_KEY_ID'] && ENV['AWS_SECRET_ACCESS_KEY']
 end
 
 
-if ENV['CLOUDINARY_URL']
-  require 'paperclip/cloudinary'
-
-  Paperclip::Attachment.default_options[:storage] = :cloudinary
-
-  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-  Paperclip::Attachment.default_options[:s3_host_name] = 'eu-west-1.amazonaws.com'
-
-
-
-  Spree::Image.attachment_definitions[:attachment].delete(:url)
-  Spree::Image.attachment_definitions[:attachment].delete(:path)
-  Spree::Image.attachment_definitions[:attachment].delete(:default_url)
-  Spree::Image.attachment_definitions[:attachment][:path] = 'spree/products/:id/:style/:filename'
-  Spree::Image.attachment_definitions[:attachment][:cloudinary_url_options] = {
-    default: { secure: true }
-  }
-end
+# if ENV['CLOUDINARY_URL']
+#   require 'paperclip/cloudinary'
+#
+#   Paperclip::Attachment.default_options[:storage] = :cloudinary
+#
+#   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#   Paperclip::Attachment.default_options[:s3_host_name] = 'eu-west-1.amazonaws.com'
+#
+#
+#
+#   Spree::Image.attachment_definitions[:attachment].delete(:url)
+#   Spree::Image.attachment_definitions[:attachment].delete(:path)
+#   Spree::Image.attachment_definitions[:attachment].delete(:default_url)
+#   Spree::Image.attachment_definitions[:attachment][:path] = 'spree/products/:id/:style/:filename'
+#   Spree::Image.attachment_definitions[:attachment][:cloudinary_url_options] = {
+#     default: { secure: true }
+#   }
+# end
