c2b6a4f78346b3b78bb380f7aedcce4dc33ecb1c
Alessio Re <alessio@xpitality.com>
2021-03-12 12:31:01 +0100

    checks for env vars

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,11 +1,13 @@
-#! /bin/bash
+#! /bin/sh
 set -e
+
+echo "environment and master key:"
+echo $RAILS_ENV
+echo $RAILS_MASTER_KEY
 
 echo "MIGRATIONS START"
 bundle exec rake db:migrate
 echo "MIGRATIONS END"
-#echo "STARTING SEEDING"
-#bundle exec rake db:seed
 
 if [[ $? != 0 ]]; then
   echo
@@ -13,6 +15,10 @@ if [[ $? != 0 ]]; then
   echo
   bundle exec rake db:setup
 fi
+
+#echo "SEEDING START"
+#bundle exec rake db:seed
+#echo "SEEDING END"
 
 # Remove a potentially pre-existing server.pid for Rails.
 if [ -f "/app/tmp/pids/server.pid" ] ; then
