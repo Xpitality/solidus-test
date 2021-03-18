e182f0235de99658a4b41030b90a421848d58fe6
Marko Bošković <bosskovic@gmail.com>
2021-03-18 17:07:19 +0100

    drops db

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -7,9 +7,7 @@ echo $RAILS_MASTER_KEY
 
 echo "MIGRATIONS START"
 RAILS_ENV=$RAILS_ENV bundle exec rake db:drop
-RAILS_ENV=$RAILS_ENV bundle exec rake db:create
-RAILS_ENV=$RAILS_ENV bundle exec rake db:migrate
-RAILS_ENV=$RAILS_ENV bundle exec rake db:seed
+#RAILS_ENV=$RAILS_ENV bundle exec rake db:migrate
 echo "MIGRATIONS END"
 
 #if [[ $? != 0 ]]; then
