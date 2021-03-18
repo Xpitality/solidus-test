e07b3494c9fedce138787277786184faf23a624f
Alessio Re <alessio@xpitality.com>
2021-03-18 18:11:55 +0100

    removed all db commands except for migrations in dev

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,30 +1,16 @@
 #! /bin/sh
 set -e
 
-echo "environment and master key:"
-echo $RAILS_ENV
-echo $RAILS_MASTER_KEY
+if [ $RAILS_ENV = "dev" ] ; then
+	echo "Running dev migrations"
+	RAILS_ENV=$RAILS_ENV bundle exec rake db:migrate
+fi
 
-echo "MIGRATIONS START"
 RAILS_ENV=$RAILS_ENV bundle exec rake db:migrate
-echo "MIGRATIONS END"
-
-#if [[ $? != 0 ]]; then
-#  echo
-#  echo "== Failed to migrate. Running setup first."
-#  echo
-#  RAILS_ENV=$RAILS_ENV bundle exec rake db:create
-#  RAILS_ENV=$RAILS_ENV bundle exec rake db:migrate
-#  RAILS_ENV=$RAILS_ENV bundle exec rake db:seed
-#fi
-
-#echo "SEEDING START"
-#RAILS_ENV=$RAILS_ENV bundle exec rake db:seed
-#echo "SEEDING END"
 
 # Remove a potentially pre-existing server.pid for Rails.
 if [ -f "/app/tmp/pids/server.pid" ] ; then
-    rm -f /app/tmp/pids/server.pid
+    	rm -f /app/tmp/pids/server.pid
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
