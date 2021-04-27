3705e0b11a3dd7577fcc3e5081c6e095fd4d9942
Marko Bošković <bosskovic@gmail.com>
2021-04-27 12:01:27 +0200

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'
   mount Spree::Core::Engine, at: '/'
-  mount Alchemy::Engine => '/'
+  mount Alchemy::Engine => '/alchemy/'
 end
 
 Spree::Core::Engine.routes.draw do
