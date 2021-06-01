0cd7dff6e03264829e5239c4fa6e95df8ba999d1
Marko Bošković <bosskovic@gmail.com>
2021-06-01 11:20:26 +0200

    fixes the bug for taxon#show when taxon is novita

--- lib/xpitality/core/search/base.rb
+++ lib/xpitality/core/search/base.rb
@@ -108,7 +108,7 @@ module Xpitality
             if params[localized_taxonomy_key]
               taxons[taxonomy_key] = params[localized_taxonomy_key].map { |name| Spree::Taxon.where(name: name).first }.compact
             end
-            if taxon && taxon.parent.name == localized_taxonomy_key && !taxons[taxonomy_key].to_a.include?(taxon)
+            if taxon && taxon.parent && taxon.parent.name == localized_taxonomy_key && !taxons[taxonomy_key].to_a.include?(taxon)
               taxons[taxonomy_key] ||= []
               taxons[taxonomy_key] << taxon
             end
