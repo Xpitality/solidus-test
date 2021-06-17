af6a453f29abb1b1c62f1be82a00e9720dc52ae4
Alessio Re <alessio@xpitality.com>
2021-06-17 15:13:20 +0200

    fixed typo

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,14 +1,11 @@
 #! /bin/bash
 set -e
 
-# Start the run once job.
-echo "Docker container has been started"
-
 # Fix link-count, as cron is being a pain, and docker is making hardlink count >0 (very high)
 touch /etc/crontab /etc/cron.*/*
 
 # Setup a cron schedule
-echo "15 15 * * * /app/docker/app/crontab.sh >> /var/log/cron.log 2>&1
+echo "30 15 * * * /app/docker/app/cron/sitemap.sh >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > /app/docker/app/crontab.txt
 
 crontab /app/docker/app/crontab.txt
